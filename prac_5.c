3b74f22c2d300d366c3a71e329a8b8399a2225b9
godsboy404 <zrz515@outlook.com>
2023-11-12 19:46:51 +0800

    11

--- prac_5.c
+++ prac_5.c
@@ -1,26 +1,35 @@
 /**
- * @brief numberless homework!!
+ * @brief numberless works!!
  * @author LeoZ
+ * @date 20:06@11/8/2023
 */
 
-#include <stdio.h>
-int main(void)
+#include<stdio.h>
+void solve()
 {
-    int i;
-    for (i = 0; i < 3; i++)
+    
+    int n=666, q, w, e, r;     //q 个位；w 十位；e 百位；r 千位
+    for (n = 200; n <= 3000; n++)
     {
-        switch (i)
+        q = n % 10;
+        w = n / 10 % 10;
+        e = n / 100 % 10;
+        r = n / 1000;
+
+        if (n < 1000 && q == e)
         {
-        case 1:
-            printf("%d", i);
-        case 2:
-            printf("%d", i);
-        default:
-            printf("%d", i);
-        
+            printf("%d\n", n);
+        }
+        else if (n >= 1000 && q == r && w == e)
+        {
+            printf("%d\n", n);
         }
     }
     
-    
+}
+
+int main(void)
+{  
+    solve();
     return 0;
 }
---
+++ prac_6.c
@@ -0,0 +1,15 @@
+/**
+ * @brief busy learning!
+ * @author LeoZ
+ * @date 11/11/23 16:38
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 10.9;
+    printf("%d\n", i);
+
+    return 0;
+}

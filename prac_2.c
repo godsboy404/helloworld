8100bd2647ccbbe58545a72c76c13a4dc4057167
godsboy404 <zrz515@outlook.com>
2023-11-15 18:40:37 +0800

    helloworld

--- prac_2.c
+++ prac_2.c
@@ -1,28 +1,11 @@
 #include<stdio.h>
 	int main(void)
-	{  
-	  int num1, num2=1, MAXCOUNT, result=0;
-    scanf("%d", &MAXCOUNT);
-
-    if (MAXCOUNT < 0)
-    {
-      printf("0");
-    }
+	{
+    int n;
+    int *h = &n;
+    scanf("%d", &n);
+    printf("%d", n);
+    printf("%d", *h);
     
-    else if (MAXCOUNT == 0)
-    {
-      printf("1");
-    }
-    
-    else
-    {
-      for ( num1 = 1; num1 <= MAXCOUNT; num1++)
-      {
-        num2 = num1 * num2;
-        result += num2;
-      }
-      printf("%d", result);
-    }
-
     return 0;
-	}
+  }
--- prac_5.c
+++ prac_5.c
@@ -1,35 +1,13 @@
 /**
- * @brief numberless works!!
- * @author LeoZ
- * @date 20:06@11/8/2023
+ * @brief s1mp prac
+ * @author Lz
+ * @date 11/15/23
 */
 
-#include<stdio.h>
-void solve()
+#include <stdio.h>
+int main()
 {
-    
-    int n=666, q, w, e, r;     //q 个位；w 十位；e 百位；r 千位
-    for (n = 200; n <= 3000; n++)
-    {
-        q = n % 10;
-        w = n / 10 % 10;
-        e = n / 100 % 10;
-        r = n / 1000;
-
-        if (n < 1000 && q == e)
-        {
-            printf("%d\n", n);
-        }
-        else if (n >= 1000 && q == r && w == e)
-        {
-            printf("%d\n", n);
-        }
-    }
-    
-}
-
-int main(void)
-{  
-    solve();
-    return 0;
+    int x = 020;
+    printf("%d", x);
+    return x;
 }

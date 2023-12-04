6314d8240f267f3758868d849a50dd4c1bbdb175
润泽 <zrz515@outlook.com>
2023-12-04 08:26:00 +0800

    Update prac_2.c: Added new string and removed unnecessary code

--- prac_2.c
+++ prac_2.c
@@ -1,35 +1,10 @@
-#include <stdio.h>
+#include<string.h>
+#include<stdio.h>
 
-int main() {
-    int characterCount = 0;
-    int lineCount = 0;
-    int wordCount = 0;
-    char ch;
-    int inWord = 0;
-
-    printf("请输入文本；键入“EOF”以结束输入，回车显示所求：\n");
-
-    while ((ch = getchar()) != EOF) 
-    {
-        characterCount++;
-
-        if (ch == '\n') 
-        {
-            lineCount++;
-        }
-
-        if (ch == ' ' || ch == '\n' || ch == '\t') 
-        {
-            inWord = 0;
-        } 
-        else if (inWord == 0) 
-        {
-            inWord = 1;
-            wordCount++;
-        }
-    }
-
-    printf("字符数：%d\n行数：%d\n单词数：%d\n", characterCount, lineCount, wordCount);
-
-    return 0;
+int main(void)
+{
+	char hello[] = "Another Hello";
+	printf("%s\n", hello);
+	getchar();    //接收个字符 防止控制台直接结束
+	return 0;
 }

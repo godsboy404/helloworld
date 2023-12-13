5607a490f8e286eaca47bc25b3863660c6e2573e
润泽 <zrz515@outlook.com>
2023-12-13 19:01:20 +0800

    Refactor code to concatenate two strings

--- prac_4.c
+++ prac_4.c
@@ -1,36 +1,13 @@
-//统计输入的字符串的行数，单词数，字符数
 #include <stdio.h>
-#include <ctype.h>
-#include <stdbool.h>
-#define STOP '|'
-int main(void)
-{
-    char c;
-    char prev;
-    long n_chars = 0L;
-    int n_lines = 0;
-    int n_words = 0;
-    int p_lines = 0;
-    bool inword = false;
-    printf("Enter text to be analyzed(| to terminate):\n");
-    prev = '\n';
-    while ((c = getchar()) != STOP)
-    {
-        n_chars++;
-        if (c == '\n')
-            n_lines++;
-        if (!isspace(c) && !inword)
-        {
-            inword = true;
-            n_words++;
-        }
-        if (isspace(c) && inword)
-            inword = false;
-        prev = c;
-    }
-    if (prev != '\n')
-        p_lines = 1;
-    printf("characters = %ld, words = %d, lines = %d, ", n_chars, n_words, n_lines);
-    printf("partial lines = %d\n", p_lines);
+#include <string.h>
+
+int main() {
+    char str1[100] = {"Hello"};
+    char str2[] = " World!";
+    
+    strcat(str1, str2);
+    
+    printf("%s\n", str1);
+    
     return 0;
 }
--- prac_7.c
+++ prac_7.c
@@ -1,14 +1,5 @@
 // 用指针交换两个整数的值
 #include <stdio.h>
-<<<<<<< HEAD
-int main()
-{
-	char i;
-	scanf("%c", &i);
-	printf("  %c\n %c%c%c\n%c%c%c%c%c\n", i, i, i, i, i, i, i, i, i);
-	
-	return 0;
-=======
 
 void swap(int *a, int *b) {
     int temp = *a;
@@ -24,5 +15,4 @@ int main() {
     swap(&a, &b);
     printf("交换后的值：a = %d, b = %d\n", a, b);
     return 0;
->>>>>>> ffc1040eecf0c94454498bf606f18878ce61a113
 }

64c0ce22a9f2f856ae5096a3c977e0d80188f9b6
润泽 <zrz515@outlook.com>
2023-12-14 19:26:24 +0800

    Add new files and modify code in prac_4.c

--- prac_4.c
+++ prac_4.c
@@ -1,14 +1,10 @@
+/**
+ * @brief ate dinner tonight..
+*/
+
 #include <stdio.h>
 
 int main() {
-    FILE *file = fopen("in.txt", "w");
-    if (file == NULL) {
-        printf("无法打开文件\n");
-        return 1;
-    }
-
-    fprintf(file, "HelloWorld");
-
-    fclose(file);
+    printf("Hello, world!\n");
     return 0;
 }

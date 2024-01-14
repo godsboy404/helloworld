83eeaf5a446c38c33e122cb69f15959c6a65a2d4
润泽 <zrz515@outlook.com>
2024-01-14 18:48:35 +0800

    Refactor code to remove unused variables and functions

--- prac_8.c
+++ prac_8.c
@@ -1,29 +1,5 @@
-#include<stdio.h>
-#include<string.h>
-char a[100]={0};
-int b;
-char* i=a;
-char* j;
-char c;
-void reverse()
-{
-    while(i<j)
-    {
-        c= *i;
-        *i= *j;
-        *j=c;
-        i++;
-        j--;
-    }
-}
-int main()
-{
-    printf("put in strings:");
-    gets(a);
-    b = strlen(a) - 1;
-    j = a + b;
-    reverse();
-    puts(a);
+#include <stdio.h>
+int main() {
     
     return 0;
 }
